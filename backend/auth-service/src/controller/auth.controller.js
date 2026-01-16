735ff7e383b384b65d0a686fd9e9c82c29105599
saquibsayyedcoder <saquibsayyed12345@gmail.com>
2026-01-16 23:28:44 +0530

    the auth service backend logic is done

---
+++ backend/auth-service/src/controller/auth.controller.js
@@ -0,0 +1,74 @@
+import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client";
+import { generateToken } from "../utils/jwt.js"
+
+const prisma = new PrismaClient();
+
+//Register controller
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password)
+      return res.status(400).json({
+        message: "All the fields required",
+      });
+
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser)
+      return res.status(400).json({
+        message: "User already Exists",
+      });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: { name, email, password: hashedPassword },
+    });
+
+    const token = generateToken({ id: user.id, role: user.role });
+    res.status(201).json({ token, user: { id: user.id, name, email } });
+  } catch (error) {
+   console.error("REGISTER ERROR:", error);
+  res.status(500).json({ error: error.message });
+  }
+};
+
+//Login
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({ message: "Email & password required" });
+
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user)
+      return res.status(404).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const token = generateToken({ id: user.id, role: user.role });
+
+    res.json({ token, user: { id: user.id, name: user.name, email } });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
+};
+
+// Logout
+export const logout = async (req, res) => {
+  try {
+    // JWT is stateless – logout is handled on client side
+    return res.status(200).json({
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Logout failed",
+    });
+  }
+};
