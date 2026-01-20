16fb49b3b358bc055644f12fa67ad2be63d0c18b
saquibsayyedcoder <saquibsayyed12345@gmail.com>
2026-01-20 16:09:12 +0530

    the payment service has been done api is working fine

---
+++ backend/payment-service/src/middleware/auth.middleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer "))
+    return res.status(401).json({ message: "Token missing" });
+
+  try {
+    const token = authHeader.split(" ")[1];
+    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
